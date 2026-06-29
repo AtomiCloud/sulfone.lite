ffa8d9eae36f341b2bba10a322d0e940796c871e
adelphi-liong <adelphi@atomi.cloud>
2026-06-28 18:30:40 -0700

    ci: configure pnpm semantic release install

--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 rm .git/hooks/* 2>/dev/null || true
+export pnpm_config_ignore_workspace_root_check=true
+export pnpm_config_dangerously_allow_all_builds=true
 sg release -i pnpm
